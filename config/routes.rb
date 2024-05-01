7b8ea64b317d571ea14485098d536c4e95265832
Philipe Leandro <philipe-leandro@hotmail.com>
2024-05-01 17:04:42 -0300

    Add devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ require "sidekiq/web"
 Rails.application.routes.draw do
   devise_for :users
   devise_for :admins
+
   get "up" => "rails/health#show", as: :rails_health_check
 
   mount Sidekiq::Web => '/sidekiq'
